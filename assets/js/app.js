5333c4f57dfde845f680006bd1cc56fd5be37b94
William I. Olojede <ilerioluwase97@gmail.com>
2017-05-01 04:53:56 +0300

    implemented single pics at once

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,73 @@
-const catImgs = selector('.catImg');
-const scoreSpans = selector('span');
-const catsH2 = selector('.cat-name');
 
 // element selector
 function selector(target) {
   return document.querySelectorAll(target);
 }
 
-//Cat names
-const cats = [
-  { name: 'spencer', score: 0}, 
-  {name: 'polly', score: 0}
-];
+const catsH2 = selector('.cat-name');
+const catsDIV = selector('.cat-wrapper');
 
-// insert cats name and score
-cats.forEach((cat, i) => {
-  catsH2[i].textContent = cat.name;
-  insertCatScore(i, cat.score);
+// cat object creator
+function Cat(name, score = 0){
+  return {
+    name: name,
+    score: score,
+  };
+}
+
+//Cat names
+const cats = [];
+
+const catNamesURL = '/assets/js/cat-names.json';
+$.getJSON(catNamesURL, (names) => {
+  // creates list by filtering names for the first 10
+  const catNames = names.filter((_name, i) => i < 10);
+
+  catNames.forEach((name)=>{
+    cats.push(Cat(name));
+  });
+
+  // list out cat names
+  // creates one list item given some text
+  function item(text) {  
+      return `<li class="catName">${text}</li>`;
+  }
+
+  //maps each name into li tag then join the array created by map into a string then insert that into the ul tag
+  const catsList = `
+    <ul class="catsNames">
+      ${ catNames.map(item).join('') }
+    </ul>
+  `;
+
+  document.querySelector('.catsName-list').innerHTML = catsList;
+
+  const catsLI = document.querySelectorAll('.catName');
+
+  // display cat image and touch count on cat name click
+  catsLI.forEach((li, i) => {
+    li.addEventListener('click', (e) => {
+      const catName = e.target.textContent;
+      const selectedCat = cats.filter((cat) => cat.name === catName);
+      catsDIV[0].innerHTML = `
+        <div class="cat">
+          <header>
+            <h2 class="cat-name">${selectedCat[0].name}</h2>
+          </header>
+          <img src="assets/img/cat${i}.jpg" class="catImg">
+          <div><h3>Number of Clicks: <span>${selectedCat[0].score}</span></h3></div>
+        </div>
+      `;
+      const catImg = selector('.catImg');
+      catImg[0].addEventListener('click', (e) => {
+        cats[i].score += 1;
+        insertCatScore(0, cats[i].score);
+      }, false);
+    }, false);
+  });
 });
 
 function insertCatScore(i, score) {
-  scoreSpans[i].textContent = score;
+  const scoreSpan = selector('span');
+  scoreSpan[i].textContent = score;
 }
-
-catImgs.forEach((catImg, i) => {
-  catImg.addEventListener('click', (e) => {
-    cats[i].score += 1;
-    insertCatScore(i, cats[i].score);
-  }, false);
-});
