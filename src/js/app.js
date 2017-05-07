1b2699e77d023a70e8d2ad63909a7fc97e3fc4a6
William I. Olojede <ilerioluwase97@gmail.com>
2017-05-07 18:37:34 +0300

    implemented form validation, warning is showed only in the console

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 const model = {
   // index of the current cat
   currentCatIndex: null,
+  adminMode: false,
   cats: [
     {
       name: 'Abby',
@@ -45,9 +46,23 @@ const controller = {
   incrementCatScore: function() {
     this.getCurrentCat().score += 1;
     views.catView.insertCatScore(this.getCurrentCat().score);
-  }, 
+  },
+  onAdminMode: function() {
+    model.adminMode = true;
+    views.adminView.render(model.adminMode);
+  },
+  offAdminMode: function() {
+    model.adminMode = false;
+    views.adminView.render(model.adminMode);
+  },
+  addNewCat: function(data){
+    const newCat = new helpers.objectCreator(data[0], data[1]);
+    model.cats.push(newCat);
+    views.catsListView.init();
+  },
   init: function() {
     views.catsListView.init();
+    views.adminView.init();
   },
 };
 
@@ -66,7 +81,6 @@ const views = {
         li.addEventListener('click', (e) => {
           controller.setCurrentCat(i);
           views.catView.init();
-          // console.log(this)
         }, false);
       });
     },
@@ -105,6 +119,47 @@ const views = {
       helpers.selector('.cat-wrapper')[0].innerHTML = this.cat;
     },  
   },
+  adminView: {
+    init: function() {
+      // add event listner to admin button and set admin mode to true
+      helpers.selector('.admin__btn')[0].addEventListener('click', (e) => {
+        controller.onAdminMode();
+      }, false);
+      // event listner on both cancel and ssave button
+      Array.from( helpers.selector('.admin-form__btn') ).forEach( (btn) => {
+        btn.addEventListener('click', (e) => {
+          e.preventDefault();
+          const adminForm = helpers.selector('.admin__form')[0];
+          if( btn.textContent === 'Cancel'){
+            // clears inputs and set admin mode to false
+            helpers.clearFormField(adminForm);
+            controller.offAdminMode();
+          } else if(btn.textContent === 'Save') {
+            const newCatDetails = helpers.getValuesOfFormField(adminForm);
+            // form validation
+            if(helpers.formValidation(newCatDetails)){
+              // pass data gotten from input field to the controller
+              controller.addNewCat(newCatDetails);
+            } else {
+              console.log('some or all of your input is invalid, please try again');
+            }
+            // clears input field
+            helpers.clearFormField(adminForm);
+          }
+        }, false)
+      });
+    },
+    render: function(flag) {
+      const adminDIV = helpers.selector('.admin')[0];
+      // makes form visible
+      if (flag && !adminDIV.classList.contains('active')){
+        adminDIV.classList.add('active');
+      }
+      if(!flag && adminDIV.classList.contains('active')){
+        adminDIV.classList.remove('active');
+      }
+    },
+  },
 };
 
 // helpers
@@ -116,8 +171,47 @@ const helpers = {
   // selects elements from the dom
   selector: function (target) {
     return document.querySelectorAll(target);
-  }
+  },
+  objectCreator: function(name, url, score = 0){
+    return {
+      name: name,
+      url: url,
+      score: score,
+    };
+  },
+  // clears form field
+  clearFormField: function(formElement){
+    // creates an iterateable array from htmlCollection
+    Array.from(formElement.getElementsByTagName('input'))
+      .forEach((tag) => tag.value = '');
+  },
+  // creates an array of all form input's value, also removes white spaces
+  getValuesOfFormField: function(formElement){
+    return Array.from(formElement.getElementsByTagName('input')).map((tag) => tag.value.trim());
+  },
+  formValidation: function(data) {
+    const catname = data[0];
+    const url = data[1];
+    // check if input is empty
+    function isFormFieldEmpty() {
+      if(catname === '' || url === ''){
+        return true;
+      }
+      return false;
+    }
+    // checks if valid image url is entered
+    function isUrlValid() {
+      if(url.match(/\.(jpeg|jpg|gif|png)$/) != null ) {
+        return true;
+      }
+      return false;
+    }
+    if (!isFormFieldEmpty() && isUrlValid()) {
+      return true;
+    }
+    return false;
+  },
 }
 
 // initialize the app
-views.catsListView.init();
+controller.init();
